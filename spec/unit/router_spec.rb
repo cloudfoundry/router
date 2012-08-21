2072526e5625884a86f0b42b07cb330bf9172b59
resouer <resouer@163.com>
2012-08-21 20:46:07 +0800

    add param to unit test
    Change-Id: I3a0dd0719c38ead91988924f813d5e00a78f76ed

--- spec/unit/router_spec.rb
+++ spec/unit/router_spec.rb
@@ -31,13 +31,13 @@ describe Router do
     end
 
     it 'should register a droplet' do
-      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2222, {}, 123)
+      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2222, {}, 123 , {})
       VCAP::Component.varz[:droplets].should == 1
       VCAP::Component.varz[:urls].should == 1
     end
 
     it 'should allow proper lookup' do
-      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2222, {}, 123)
+      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2222, {}, 123, {})
       droplets = Router.lookup_droplet('foo.vcap.me')
       droplets.should be_instance_of Array
       droplets.should have(1).items
@@ -49,46 +49,48 @@ describe Router do
       droplet.should have_key :host
       droplet.should have_key :port
       droplet.should have_key :clients
+	  droplet.should have_key :res_usage
       droplet[:requests].should == 0
       droplet[:url].should == 'foo.vcap.me'
       droplet[:host].should == '10.0.1.22'
       droplet[:port].should == 2222
       droplet[:clients].should == {}
+	  droplet[:res_usage].should == {}
     end
 
     it 'should allow looking up uppercase uri' do
-      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2222, {}, 123)
+      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2222, {}, 123, {})
       droplets = Router.lookup_droplet('FOO.VCAP.ME')
       droplets.should be_instance_of Array
       droplets.should have(1).items
     end
 
     it 'should count droplets independent of URL' do
-      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2222, {}, 123)
-      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2224, {}, 123)
+      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2222, {}, 123, {})
+      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2224, {}, 123, {})
       VCAP::Component.varz[:droplets].should == 2
       VCAP::Component.varz[:urls].should == 1
     end
 
     it 'should return multiple droplets for a url when they exist' do
-      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2222, {}, 123)
-      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2224, {}, 123)
+      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2222, {}, 123, {})
+      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2224, {}, 123, {})
       droplets = Router.lookup_droplet('foo.vcap.me')
       droplets.should be_instance_of Array
       droplets.should have(2).items
     end
 
     it 'should ignore duplicates' do
-      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2222, {}, 123)
-      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2224, {}, 123)
-      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2224, {}, 123)
+      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2222, {}, 123, {})
+      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2224, {}, 123, {})
+      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2224, {}, 123, {})
       VCAP::Component.varz[:droplets].should == 2
       VCAP::Component.varz[:urls].should == 1
     end
 
     it 'should record tags' do
       VCAP::Component.varz[:tags]["component"].should be_nil
-      Router.register_droplet('foobar.vcap.me', '10.0.1.22', 2225, {"component" => "test"}, 123)
+      Router.register_droplet('foobar.vcap.me', '10.0.1.22', 2225, {"component" => "test"}, 123, {})
       VCAP::Component.varz[:tags]["component"]["test"].should_not be_nil
       droplets = Router.lookup_droplet('foobar.vcap.me')
       droplets.first[:tags].should == {"component" => "test"}
@@ -102,30 +104,30 @@ describe Router do
     end
 
     it 'should unregister a droplet' do
-      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2222, {}, 123)
-      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2224, {}, 123)
+      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2222, {}, 123, {})
+      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2224, {}, 123, {})
       Router.unregister_droplet('foo.vcap.me', '10.0.1.22', 2224)
       VCAP::Component.varz[:droplets].should == 1
       VCAP::Component.varz[:urls].should == 1
     end
 
     it 'should unregister a droplet that had tags' do
-      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2224, {}, 123)
-      Router.register_droplet('foobar.vcap.me', '10.0.1.22', 2225, {"component" => "test"}, 123)
+      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2224, {}, 123, {})
+      Router.register_droplet('foobar.vcap.me', '10.0.1.22', 2225, {"component" => "test"}, 123, {})
       Router.unregister_droplet('foobar.vcap.me', '10.0.1.22', 2225)
       VCAP::Component.varz[:droplets].should == 1
       VCAP::Component.varz[:urls].should == 1
     end
 
     it 'should not return unregistered items' do
-      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2222, {}, 123)
+      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2222, {}, 123, {})
       Router.unregister_droplet('foo.vcap.me', '10.0.1.22', 2222)
       droplets = Router.lookup_droplet('foo.vcap.me')
       droplets.should be_nil
     end
 
     it 'should properly account for urls and droplets' do
-      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2222, {}, 123)
+      Router.register_droplet('foo.vcap.me', '10.0.1.22', 2222, {}, 123, {})
       Router.unregister_droplet('foo.vcap.me', '10.0.1.22', 2222)
       VCAP::Component.varz[:droplets].should == 0
       VCAP::Component.varz[:urls].should == 0
